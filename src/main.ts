f81c8ba42788a028eb7084f9ce34add7ff75a482
9bfish8 <9bfish8@gmail.com>
2025-01-18 16:41:32 +0900

    배포 도메인 설정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', // 프론트엔드 주소
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://princesses-frontend.vercel.app'  // 추가된 Vercel 도메인
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
